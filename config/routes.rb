8f755eac97564ebb87b69ae6423b58f43d0967cc
Yuncong Yang <yy3035@columbia.edu>
2022-11-02 02:59:32 +0000

    add things to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root :to => 'sessions#index'
+  # Routes for Google authentication
+  get 'auth/:provider/callback', to: 'sessions#googleAuth'
+  get 'auth/failure', to: redirect('/')
 end
